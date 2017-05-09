bbd7e0e61b74b61b7a2a8b387ef64ed232216b9b
Suraj Awal <suraj.awal10@gmail.com>
2017-05-09 21:00:14 +0545

    sum program committed

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+{
+	int a, b, sum; //Declaring variables as integers
+	printf("Enter two numbers ::: ");
+	scanf("%d %d", &a, &b); //Taking two integers as input
+	printf("\n\n");
+	sum = a + b; //Calculating the sum
+	printf("The sum is %d. \n", sum); //Displaying the final output
+}
